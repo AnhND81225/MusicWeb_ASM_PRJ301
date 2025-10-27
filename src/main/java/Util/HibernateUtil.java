737dc2a22ebfec316fd67b39b67ca188c72afef9
ducanh.nguyenkl@gmail.com <nguyenducanh@example.com>
2025-10-27 09:04:03 +0700

    Test success, Fix HibernateUti, hibernate.cfg.xml, pom.xml

--- src/main/java/Util/HibernateUtil.java
+++ src/main/java/Util/HibernateUtil.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Util;
 
-/* day la class de quản lý SessionFactory*/
-import static jdk.nashorn.internal.objects.NativeError.printStackTrace;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- *
- * @author ASUS
- */
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
@@ -21,8 +11,8 @@ public class HibernateUtil {
         try {
             return new Configuration().configure().buildSessionFactory();
         } catch (Exception e) {
-            printStackTrace(e);
-            return null;
+            e.printStackTrace(); // In lỗi ra console
+            throw new ExceptionInInitializerError(e); // Ném lỗi để dừng chương trình
         }
     }
 
